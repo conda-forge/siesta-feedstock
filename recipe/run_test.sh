9ef64252ded8393d529334c740388fe1f32c573b
Nick Papior <nickpapior@gmail.com>
2018-08-17 21:13:43 -0400

    trying a real example for testing
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# Test command:
-cat <<EOF > RUN.fdf
-Do nothing
-EOF
-
-siesta < RUN.fdf || echo SHOULD FAIL
-rm RUN.fdf
+# Run H2O system
+mkdir h2o
+cd h2o
+cp $SRC_DIR/Tests/Pseudos/H.psf .
+cp $SRC_DIR/Tests/Pseudos/O.psf .
+cp $SRC_DIR/Tests/h2o/h2o.fdf .
+siesta < h2o.fdf > h2o.out
+cd ..
