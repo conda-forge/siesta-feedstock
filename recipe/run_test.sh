33fb92ae0eaba03934545cd2adf9d96448d2c6a6
Nick Papior <nickpapior@gmail.com>
2018-08-17 21:55:56 -0400

    trying to fix test
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
-
+set -e
 ls -l
+
+for cmd in
+    siesta transiesta tbtrans
+    eigfact2plot gnubands mprop fat
+    denchar Eig2DOS
+    grid2cube grid_rotate grid_supercell
+    fcbuild vibra
+    mixps fractional
+    readwf readwfx info_wfsx wfs2wfsx wfsx2wfs
+do
+    command -v $cmd
+done
+
+export OMPI_MCA_plm=isolated
+export OMPI_MCA_btl_vader_single_copy_mechanism=none
+export OMPI_MCA_rmaps_base_oversubscribe=yes
 
 # Run H2O system
 mkdir h2o
@@ -8,5 +24,6 @@ cd h2o
 cp ../Tests/Pseudos/H.psf .
 cp ../Tests/Pseudos/O.psf .
 cp ../Tests/h2o/h2o.fdf .
-siesta < h2o.fdf > h2o.out
+mpirun --allow-run-as-root siesta < h2o.fdf > h2o.out
 cd ..
+
