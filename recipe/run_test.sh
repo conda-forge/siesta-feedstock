c867e3c9a1021076b8caa72031c2cf85252c3e81
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-18 12:39:34 +0200

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,16 +4,10 @@ set -e
 echo "Running tests"
 ls -l
 
-# for cmd in siesta transiesta tbtrans \
-# 		  eigfat2plot gnubands mprop fat \
-# 		  denchar Eig2DOS \
-# 		  grid2cube grid_rotate grid_supercell \
-# 		  fcbuild vibra \
-# 		  mixps fractional \
-# 		  readwf readwfx info_wfsx wfs2wfsx wfsx2wfs
-# do
-#     command -v $cmd
-# done
+for cmd in siesta transiesta eigfat2plot gnubands mprop fat denchar Eig2DOS grid2cube grid_rotate grid_supercell fcbuild vibra mixps fractional readwf readwfx info_wfsx wfs2wfsx wfsx2wfs
+do
+    command -v $cmd
+done
 
 export OMPI_MCA_plm=isolated
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
