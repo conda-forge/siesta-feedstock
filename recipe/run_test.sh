7b41d6d29e3b2f4fc02e61966e9aa2516c9b8c9c
zerothi <nickpapior@gmail.com>
2021-04-16 21:19:03 +0200

    fixed test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,11 +4,11 @@ set -e
 echo "Running tests"
 ls -l
 
-for cmd in siesta transiesta tbtrans \
+for cmd in siesta tbtrans \
 		  eigfat2plot gnubands mprop fat \
 		  denchar Eig2DOS \
 		  grid2cube grid_rotate grid_supercell \
-		  fcbuild vibra \
+		  fcbuild vibra mprop \
 		  mixps fractional \
 		  readwf readwfx info_wfsx wfs2wfsx wfsx2wfs
 do
