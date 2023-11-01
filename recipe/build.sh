2ca4f4913f08bb22100afd298eac34875ae8f986
zerothi <nickpapior@gmail.com>
2023-11-01 09:01:16 +0100

    fixed mac builds
    Signed-off-by: zerothi <nickpapior@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# error on faulty execution
+set -ex
+
 # Remove __FILE__ lines in utils file.
 sed -i -e "s:__FILE__:'fdf/utils.F90':g" Src/fdf/utils.F90
 
@@ -9,28 +12,43 @@ echo "Runing with mpi=$mpi and blas=$blas_impl"
 cd Obj
 ../Src/obj_setup.sh
 
+if [[ "$target_platform" == linux-* || "$target_platform" == "osx-arm64"  ]]; then
+  # Workaround for https://github.com/conda-forge/scalapack-feedstock/pull/30#issuecomment-1061196317
+  # As of March 2022, on macOS (Intel) gfortran 9 is still used
+  export FFLAGS="$FFLAGS -fallow-argument-mismatch"
+  export DEBUG_FFLAGS="$DEBUG_FFLAGS -fallow-argument-mismatch"
+  export OMPI_FCFLAGS="$FFLAGS"
+fi
 
-# Get the gcc version
-echo "GCC version string: $(gcc --version | head -1)"
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  # This is only used by open-mpi's mpicc
+  # ignored in other cases
+  export OMPI_CC=$CC
+  export OMPI_CXX=$CXX
+  export OMPI_FC=$FC
+  export OPAL_PREFIX=$PREFIX
+fi
 
-# In 4.0 we do not use OpenMP!
-if [[ -n "$GCC" ]]; then
-    repl="s:%CC%:$GCC:g"
-    gcc_version=$($GCC --version | head -1 | awk '{print $NF}')
+if [[ "$(uname)" == "Darwin" ]]; then
+  # Fix headerpad-max-install-error
+  # install_name_tool: changing install names or rpaths can't be redone for
+  #  (for architecture x86_64) because larger updated load commands do not fit (the program must be relinked, and you may need to use -headerpad or -headerpad_max_install_names)
+  export SONAME="-Wl,-install_name,@rpath/"
+  export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
 else
-    repl="s:%CC%:$CC:g"
-    gcc_version=$($CC --version | head -1 | awk '{print $NF}')
+  export SONAME="-Wl,-soname,"
 fi
-repl="$repl;s:%CFLAGS%:${CFLAGS//-fopenmp/}:g"
 
-# Check if we have a gcc version >= 10
-echo "GCC version [1] == $gcc_version"
-gcc_version=${gcc_version%%.*}
-echo "GCC version [2] == $gcc_version"
-if [[ $gcc_version -ge 10 ]]; then
-    export FFLAGS="$FFLAGS -fallow-argument-mismatch"
-    export DEBUG_FFLAGS="$DEBUG_FFLAGS -fallow-argument-mismatch"
+if [[ "$mpi" != "nompi" ]]; then
+  # This is not necessary as the arch.make files
+  # handles this correctly
+  echo "passing on setting CC and FC for non-mpi"
+  #export CC=mpicc
+  #export FC=mpifort
 fi
+
+# Get the version
+echo "CC version string: $($CC --version | head -1)"
 
 if [[ -n "$GCC_AR" ]]; then
     repl="$repl;s:%AR%:$GCC_AR:g"
@@ -42,16 +60,19 @@ if [[ -n "$GCC_RANLIB" ]]; then
 else
     repl="$repl;s:%RANLIB%:$RANLIB:g"
 fi
+repl="$repl;s:%CC%:$CC:g"
 repl="$repl;s:%FC%:$FC:g"
+# No OpenMP!
+repl="$repl;s:%CFLAGS%:${CFLAGS//-fopenmp/}:g"
 repl="$repl;s:%FFLAGS%:${FFLAGS//-fopenmp/}:g"
 repl="$repl;s:%FFLAGS_DEBUG%:${DEBUG_FFLAGS//-fopenmp/}:g"
 repl="$repl;s:%INCFLAGS%:-I$PREFIX/include:g"
-repl="$repl;s:%LDFLAGS%:-L$PREFIX/lib:g"
+repl="$repl;s:%LDFLAGS%:-L$PREFIX/lib $LDFLAGS:g"
 
 if [[ "$mpi" == "nompi" ]]; then
-    sed -e "$repl" $RECIPE_DIR/arch.make.SEQ > arch.make
+  sed -e "$repl" $RECIPE_DIR/arch.make.SEQ > arch.make
 else
-    sed -e "$repl" $RECIPE_DIR/arch.make.MPI > arch.make
+  sed -e "$repl" $RECIPE_DIR/arch.make.MPI > arch.make
 fi
 echo "<<< arch.make >>>"
 cat arch.make
@@ -62,13 +83,13 @@ function mkcp {
     shift
     local exe=$target
     if [ $# -ge 1 ]; then
-	exe=$1
-	shift
+	    exe=$1
+	    shift
     fi
     echo "RUNNING: make $target"
     make $target
     [ -e compinfo.F90 ] && cat compinfo.F90
-    cp $target $PREFIX/bin/$exe
+    cp -av $target $PREFIX/bin/$exe
     make clean
 }
 
@@ -77,7 +98,6 @@ make libxmlparser.a
 # Try and build FoX to catch any debugs
 # make FoX/.config || cat FoX/config.log
 
-set -x
 ls -l
 make version
 cat compinfo.F90
